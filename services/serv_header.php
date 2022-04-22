c7e68e6e86a3775685f2c0b85a5dd4dcd8ddc533
Nick Spanos <nickspanosf1@gmail.com>
2022-04-22 13:07:05 +0300

    Adding images for team and create carrer page

--- services/serv_header.php
+++ services/serv_header.php
@@ -49,20 +49,20 @@
 
       <nav id="navbar" class="navbar order-last order-lg-0 ">
         <ul>
-          <li><a class="nav-link scrollto active" href="#hero">Αρχική</a></li>
-          <li><a class="nav-link scrollto" href="#about">Γνωριμία</a></li>
-          <li class="dropdown"><a href="#services"><span>Υπηρεσίες</span> <i class="bi bi-chevron-down"></i></a>
+          <li><a class="nav-link scrollto active" href="../index.html#hero">Αρχική</a></li>
+          <li><a class="nav-link scrollto" href="../index.html#about">Γνωριμία</a></li>
+          <li class="dropdown"><a href="../index.html#services"><span>Υπηρεσίες</span> <i class="bi bi-chevron-down"></i></a>
             <ul>
               <li><a href="services/health.php">Ασφάλεια Υγείας </a></li>
               <li><a href="#">Ασφάλεια Αυτοκινήτου</a></li>
               <li><a href="#">Ασφάλιση Ζωής</a></li>
             </ul>
           </li>
-          <li><a class="nav-link scrollto" href="#team">Ομάδα</a></li>
-          <li><a class="nav-link scrollto" href="#our-values">Blog</a></li>
-          <li><a class="nav-link scrollto" href="#faq">Συχνές Ερωτήσεις</a></li>
-          <li><a class="nav-link scrollto " href="#cta">Ευκαιρία Καριέρας</a></li>
-          <li><a class="nav-link scrollto" href="#contact">Επικοινωνία</a></li>
+          <li><a class="nav-link scrollto" href="../index.html#team">Ομάδα</a></li>
+          <li><a class="nav-link scrollto" href="../index.html#our-values">Blog</a></li>
+          <li><a class="nav-link scrollto" href="../index.html#faq">Συχνές Ερωτήσεις</a></li>
+          <li><a class="nav-link scrollto " href="../index.html#cta">Ευκαιρία Καριέρας</a></li>
+          <li><a class="nav-link scrollto" href="../index.html#contact">Επικοινωνία</a></li>
         </ul>
         <i class="bi bi-list mobile-nav-toggle"></i>
       </nav><!-- .navbar -->
