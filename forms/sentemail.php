6f495354de413bc24fb824622d54613a3c06f37c
ChristosBaztekas <christosbaztekas@gmail.com>
2022-06-04 22:21:03 +0300

    alla cors

--- forms/sentemail.php
+++ forms/sentemail.php
@@ -1,12 +1,17 @@
 <?php
+header('Access-Control-Allow-Origin: *');
 
+header('Access-Control-Allow-Methods: GET, POST');
+
+header("Access-Control-Allow-Headers: X-Requested-With");
 $dataform=$_POST['data'];
 
 
 // $to = "thpapathanasiou@gmail.com";
-$to = "gil@gileaders.gr";
+//gil@gileaders.gr
+$to = "christosbaztekas@gmail.com";
 $subject = "Newsletter";
-$message .= "<br>Email: ";
+$message = "<br>Email: ";
 $message .= $dataform[0]['value'];
 
 $headers = "Content-Type: text/html; charset=UTF-8; ";
